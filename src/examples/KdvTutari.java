d4d9213a2021e27d06dc8aaa82ae456ebf2ef98e
EREN <ernuygur@gmail.com>
2022-06-01 00:46:11 +0300

    Example

--- src/examples/KdvTutari.java
+++ src/examples/KdvTutari.java
@@ -1,8 +1,18 @@
 package examples;
 
+import java.util.Scanner;
 public class KdvTutari {
-    public static void main(String[] args) {
-        int i;
-        int k;
+    public static void main(String[] args)
+    {
+       Scanner kb = new Scanner(System.in);
+
+       double kdvOran = 1.18, tutar, kdvliTutar;
+
+       System.out.print("Ucret tutarini giriniz :");
+       tutar = kb.nextDouble();
+
+       kdvliTutar = tutar * kdvOran;
+
+       System.out.printf("KDV'li tutar : %.2f%n", kdvliTutar);
     }
 }
