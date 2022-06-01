8fbb921bae4b485f7e19393a1b61555782c9da0f
EREN <ernuygur@gmail.com>
2022-06-02 02:44:22 +0300

    Example

---
+++ src/examples/Taksimetre.java
@@ -0,0 +1,22 @@
+package examples;
+
+import java.util.Scanner;
+
+public class Taksimetre {
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner(System.in);
+
+        double tutar, km;
+
+        System.out.print("Kac km yol yapildi");
+        km = Double.parseDouble(kb.nextLine());
+
+        tutar = 10 + km * 2.2;
+
+        if (tutar < 20)
+            System.out.println("Odenecek ucret = 20TL");
+        else
+            System.out.printf("Odenecek ucret = %.2f%n", tutar);
+    }
+}
