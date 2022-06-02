1a6fbf2031d48144c7097d467ca8d527d25b9aaa
EREN <ernuygur@gmail.com>
2022-06-03 02:00:18 +0300

    Homework

--- src/homeworks/VucutKitleIndexi.java
+++ src/homeworks/VucutKitleIndexi.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class VucutKitleIndexi {
     public static void main(String[] args)
     {
-     Scanner kb = new Scanner(System.in);
+      Scanner kb = new Scanner(System.in);
 
-     double boy, kilo, index;
+      double boy, kilo, index;
 
-     System.out.print("Boyunuzu Giriniz (m) :");
-     boy = kb.nextDouble();
+      System.out.print("Boyunuzu Giriniz (m) :");
+      boy = kb.nextDouble();
 
-     System.out.print("Kilonuzu Giriniz (kg) :");
-     kilo = kb.nextDouble();
+      System.out.print("Kilonuzu Giriniz (kg) :");
+      kilo = kb.nextDouble();
 
-     index = kilo / (boy * boy);
+      index = kilo / (boy * boy);
 
-     System.out.printf("Vucut Kitle Endexiniz : %f%n", index);
+      System.out.printf("Vucut Kitle Endexiniz : %f%n", index);
     }
 }
