314610bfba6ab879ae7326252132baaff8287a36
EREN <ernuygur@gmail.com>
2022-06-03 04:30:15 +0300

    Example

---
+++ src/examples/KullaniciGirisi.java
@@ -0,0 +1,35 @@
+package examples;
+
+import java.util.Scanner;
+
+public class KullaniciGirisi {
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("Kullanici adinizi giriniz :");
+        String username = kb.nextLine();
+
+        System.out.print("Sifrenizi giriniz :");
+        String pass = kb.nextLine();
+
+        if (username.equals("patika") && pass.equals("1234"))
+            System.out.println("Basariyla giris yaptiniz");
+        else {
+            System.out.println("Girdiginiz sifre hatali");
+
+            System.out.print("1-Sifremi sifirlamak istiyorum\n2-Islem yapmadan devam etmek istiyorum ");
+            int select = Integer.parseInt(kb.nextLine());
+
+            if (select == 1) {
+                System.out.print("Yeni sifrenizi giriniz :");
+                String newpass = kb.nextLine();
+
+                if (newpass.equals("1234"))
+                    System.out.println("Ayni sifreyi girdiniz. Lutfen tekrar deneyin");
+                else
+                    System.out.println("Yeni sifreniz basarilya olusturuldu");
+            }
+        }
+    }
+}
