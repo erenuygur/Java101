ed51f42dac7d4decb6d43e16aa2f8c52c0b2318e
EREN <ernuygur@gmail.com>
2022-06-03 04:01:05 +0300

    Homework

---
+++ src/examples/BasitHesapMakinesi.java
@@ -0,0 +1,39 @@
+package examples;
+
+import java.util.*;
+
+public class BasitHesapMakinesi {
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner(System.in);
+
+        double x, y;
+
+        System.out.print("Ilk sayiyi giriniz :");
+        x = kb.nextDouble();
+
+        System.out.print("Ikinci sayiyi giriniz :");
+        y = kb.nextDouble();
+
+        System.out.println("1-Toplama\n2-Cikarma\n3-Carpma\n4-Bolme");
+        System.out.print("Hangi islemi yapmak istiyorsunuz :");
+        int secim = kb.nextInt();
+
+        switch (secim) {
+        case 1:
+            System.out.printf("" + (x + y));
+            break;
+        case 2:
+            System.out.printf("" + (x - y));
+            break;
+        case 3:
+            System.out.printf("" + (x * y));
+            break;
+        case 4:
+            System.out.printf("" + (x / y));
+            break;
+        default:
+            System.out.println("Gecersiz bir secim yaptiniz. Tekrar deneyin");
+        }
+    }
+}
