b6a1edf706db8cc1a4a7a921c1cc15534c3edf0c
EREN <ernuygur@gmail.com>
2022-06-04 03:29:43 +0300

    Example

---
+++ src/examples/SicakligaGoreEtkinlik.java
@@ -0,0 +1,17 @@
+package examples;
+
+import java.util.Scanner;
+
+public class SicakligaGoreEtkinlik {
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("Hava sicakligini giriniz : ");
+        int sicaklik = kb.nextInt();
+
+        String s = (sicaklik < 5 ? "Kayaga gidebilirsiniz": (sicaklik < 15 ? "Sinemaya gidebilirsiniz":(sicaklik < 25 ? "Piknige gidebilirsiniz": "Yuzmeye gidebilirsiniz")));
+
+        System.out.println(s);
+    }
+}
