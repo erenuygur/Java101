241c8de588907501e66943c68b687b437cc18474
Eren <ernuygur@gmail.com>
2022-06-14 03:54:44 +0300

    practices

--- src/practices/BurcBulmaProgrami.java
+++ src/practices/BurcBulmaProgrami.java
@@ -18,57 +18,68 @@ public class BurcBulmaProgrami {
                 System.out.println("Oglak Burcu");
             else
                 System.out.println("Kova Burcu");
-        } else if (month == 2 && day >= 1 && day <= 29) {
+        }
+        else if (month == 2 && day >= 1 && day <= 29) {
             if (day <= 19)
                 System.out.println("Kova Burcu");
             else
                 System.out.println("Balik Burcu");
-        } else if (month == 3 && day >= 1 && day <= 31) {
+        }
+        else if (month == 3 && day >= 1 && day <= 31) {
             if (day <= 20)
                 System.out.println("Balik Burcu");
             else
                 System.out.println("Koc Burcu");
-        } else if (month == 4 && day >= 1 && day <= 30) {
+        }
+        else if (month == 4 && day >= 1 && day <= 30) {
             if (day <= 20)
                 System.out.println("Koc Burcu");
             else
                 System.out.println("Boga Burcu");
-        } else if (month == 5 && day >= 1 && day <= 31) {
+        }
+        else if (month == 5 && day >= 1 && day <= 31) {
             if (day <= 21)
                 System.out.println("Boga Burcu");
             else
                 System.out.println("Ikizler Burcu");
-        } else if (month == 6 && day >= 1 && day <= 30) {
+        }
+        else if (month == 6 && day >= 1 && day <= 30) {
             if (day <= 22)
                 System.out.println("Ikizler Burcu");
             else
                 System.out.println("Yengec Burcu");
-        } else if (month == 7 && day >= 1 && day <= 31) {
+        }
+        else if (month == 7 && day >= 1 && day <= 31) {
             if (day <= 22)
                 System.out.println("Yengec Burcu");
             else
                 System.out.println("Aslan Burcu");
-        } else if (month == 8 && day >= 1 && day <= 31) {
+        }
+        else if (month == 8 && day >= 1 && day <= 31) {
             if (day <= 22)
                 System.out.println("Aslan Burcu");
             else
                 System.out.println("Basak Burcu");
-        } else if (month == 9 && day >= 1 && day <= 30) {
+        }
+        else if (month == 9 && day >= 1 && day <= 30) {
             if (day <= 22)
                 System.out.println("Basak Burcu");
             else
                 System.out.println("Terazi Burcu");
-        } else if (month == 10 && day >= 1 && day <= 31) {
+        }
+        else if (month == 10 && day >= 1 && day <= 31) {
             if (day <= 22)
                 System.out.println("Terazi Burcu");
             else
                 System.out.println("Akrep Burcu");
-        } else if (month == 11 && day >= 1 && day <= 30) {
+        }
+        else if (month == 11 && day >= 1 && day <= 30) {
             if (day <= 21)
                 System.out.println("Akrep Burcu");
             else
                 System.out.println("Yay Burcu");
-        } else if (month == 12 && day >= 1 && day <= 31) {
+        }
+        else if (month == 12 && day >= 1 && day <= 31) {
             if (day <= 21)
                 System.out.println("Yay Burcu");
             else
