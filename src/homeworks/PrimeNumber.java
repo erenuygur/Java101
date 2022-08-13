20f146ad413729755ed9682e89f11de6318eef83
Eren <ernuygur@gmail.com>
2022-08-14 01:45:17 +0300

    homework

---
+++ src/homeworks/PrimeNumber.java
@@ -0,0 +1,40 @@
+package homeworks;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("Bir sayi giriniz: ");
+        int val = Integer.parseInt(kb.nextLine());
+
+        if (isPrime(val))
+            System.out.printf("%d asal bir sayidir", val);
+        else
+            System.out.printf("%d asal bir sayi degildir", val);
+
+    }
+
+    public static boolean isPrime(int val)
+    {
+        if (val % 2 == 0)
+            return val == 2;
+
+        if (val % 3 == 0)
+            return val == 3;
+
+        if (val % 5 == 0)
+            return val == 5;
+
+        if (val % 7 == 0)
+            return val == 7;
+
+        for (int i = 11; i * i < val; i += 2)
+            if (val % i == 0)
+                return false;
+
+        return true;
+    }
+}
